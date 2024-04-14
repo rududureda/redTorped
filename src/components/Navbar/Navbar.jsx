45e200caa7241a77bbb56b496c58eb05656ee5a1
rududureda <ranc.reda@gmail.com>
2024-04-14 17:54:56 +0300

    add project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import '../Navbar/Navbar.scss'
+
+function Navbar() {
+  return (
+    <nav className="nav-container">
+      <h1>Red Tordeda</h1>
+      <ul>
+        <li>Your map</li>
+        <li>Visited Places</li>
+        <li>Future plans</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
