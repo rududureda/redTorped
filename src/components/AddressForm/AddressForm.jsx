8e4efb449592a3ae2eee73c63a70edf3b90d8ea3
rududureda <ranc.reda@gmail.com>
2024-04-18 22:12:29 +0300

    add try visited country

--- src/components/AddressForm/AddressForm.jsx
+++ src/components/AddressForm/AddressForm.jsx
@@ -1,98 +1,49 @@
-// import './addressForm.scss';
-// import AutoCompleteInput from '../AutoCompleteInput/AutoCompleteInput';
-// import PropTypes from 'prop-types';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-
-// AddressForm.propTypes = {
-//   address: PropTypes.object.isRequired,
-//   onSubmit: PropTypes.func.isRequired,
-//   setAddress: PropTypes.func.isRequired,
-// };
-
-// export default function AddressForm({ address, onSubmit, setAddress }) {
-//   const handleRemovefromCountrys=()=>{};
-//   return (
-//     <form className="form" onSubmit={onSubmit}>
-//       <label htmlFor="address">Address</label>
-//       <AutoCompleteInput setAddress={setAddress} />
-//       <div className='visitedCountry'>
-//         <div>
-//           <h2>{address.country}</h2>
-//           <FontAwesomeIcon
-//             icon={faCircleXmark}
-//             className="favorite-icon favorite-icon--active"
-//             onClick={() => {
-//               console.log('clicked');
-//             }}
-//           />
-//         </div>
-//       </div>
-//       <div className="buttons">
-//         <button type="submit" className="confirm-button">
-//           Confirm
-//         </button>
-//         <button
-//           type="reset"
-//           className="reset-button"
-//           onClick={() =>
-//             setAddress({
-//               country: '',
-//               latitude: '',
-//               longitude: '',
-//             })
-//           }
-//         >
-//           Reset
-//         </button>
-//       </div>
-//     </form>
-//   );
-// }
 import './addressForm.scss';
 import AutoCompleteInput from '../AutoCompleteInput/AutoCompleteInput';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'; // Changed the icon to faTimesCircle
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 AddressForm.propTypes = {
   address: PropTypes.object.isRequired,
   onSubmit: PropTypes.func.isRequired,
   setAddress: PropTypes.func.isRequired,
+  handleAddToVisitedCountry: PropTypes.func, //deleted .isRequired
+  handleRemoveFromVisitedCountry: PropTypes.func, //deleted .isRequired
+  visitedCountryData: PropTypes.array, //deleted .isRequired
 };
 
-export default function AddressForm({ address, onSubmit, setAddress }) {
-  // Step 1: Create state to store the list of visited countries
-  const [visitedCountries, setVisitedCountries] = useState([]);
-
-  // Step 3: Implement the handleRemovefromCountrys function
-  const handleRemovefromCountrys = (index) => {
-    const updatedCountries = [...visitedCountries];
-    updatedCountries.splice(index, 1);
-    setVisitedCountries(updatedCountries);
-  };
-
+export default function AddressForm({
+  address,
+  onSubmit,
+  setAddress,
+  handleAddToVisitedCountry,
+  handleRemoveFromVisitedCountry,
+  visitedCountryData,
+}) {
+  const isVisitedCountry = visitedCountryData.some(
+    (item) => item.address === address
+  );
   return (
     <form className="form" onSubmit={onSubmit}>
       <label htmlFor="address">Address</label>
       <AutoCompleteInput setAddress={setAddress} />
-
-      {/* Step 4: Render the list of visited countries dynamically */}
       <div className="visitedCountry">
-        {visitedCountries.map((country, index) => (
-          <div key={index}>
-            <h2>{country}</h2>
-            {/* Step 5: Add an event handler to the remove button */}
-            <FontAwesomeIcon
-              icon={faTimesCircle}
-              className="favorite-icon favorite-icon--active"
-              onClick={() => handleRemovefromCountrys(index)}
-            />
-          </div>
-        ))}
+        <div>
+          <h2>{address.country}</h2>
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            className={`favorite-icon ${
+              isVisitedCountry ? 'favorite-icon--active' : ''
+            } `}
+            onClick={() => {
+              isVisitedCountry
+                ? handleRemoveFromVisitedCountry(address.country)
+                : handleAddToVisitedCountry(address.country);
+            }}
+          />
+        </div>
       </div>
-
       <div className="buttons">
         <button type="submit" className="confirm-button">
           Confirm
@@ -114,3 +65,68 @@ export default function AddressForm({ address, onSubmit, setAddress }) {
     </form>
   );
 }
+// import './addressForm.scss';
+// import AutoCompleteInput from '../AutoCompleteInput/AutoCompleteInput';
+// import PropTypes from 'prop-types';
+// import { useState } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'; // Changed the icon to faTimesCircle
+
+// AddressForm.propTypes = {
+//   address: PropTypes.object.isRequired,
+//   onSubmit: PropTypes.func.isRequired,
+//   setAddress: PropTypes.func.isRequired,
+// };
+
+// export default function AddressForm({ address, onSubmit, setAddress }) {
+//   // Step 1: Create state to store the list of visited countries
+//   const [visitedCountries, setVisitedCountries] = useState([]);
+
+//   // Step 3: Implement the handleRemovefromCountrys function
+//   const handleRemovefromCountrys = (index) => {
+//     const updatedCountries = [...visitedCountries];
+//     updatedCountries.splice(index, 1);
+//     setVisitedCountries(updatedCountries);
+//   };
+
+//   return (
+//     <form className="form" onSubmit={onSubmit}>
+//       <label htmlFor="address">Address</label>
+//       <AutoCompleteInput setAddress={setAddress} />
+
+//       {/* Step 4: Render the list of visited countries dynamically */}
+//       <div className="visitedCountry">
+//         {visitedCountries.map((country, index) => (
+//           <div key={index}>
+//             <h2>{address.country}</h2>
+//             {/* Step 5: Add an event handler to the remove button */}
+//             <FontAwesomeIcon
+//               icon={faTimesCircle}
+//               className="favorite-icon favorite-icon--active"
+//               onClick={() => handleRemovefromCountrys(index)}
+//             />
+//           </div>
+//         ))}
+//       </div>
+
+//       <div className="buttons">
+//         <button type="submit" className="confirm-button">
+//           Confirm
+//         </button>
+//         <button
+//           type="reset"
+//           className="reset-button"
+//           onClick={() =>
+//             setAddress({
+//               country: '',
+//               latitude: '',
+//               longitude: '',
+//             })
+//           }
+//         >
+//           Reset
+//         </button>
+//       </div>
+//     </form>
+//   );
+// }
--- src/components/VisitedCountry/VisitedCountry.jsx
+++ src/components/VisitedCountry/VisitedCountry.jsx
@@ -1,61 +1,33 @@
-// import './VisitedCountry.scss';
-// import { useState } from 'react';
-// import AddressForm from '../AddressForm/AddressForm';
+import './VisitedCountry.scss';
+import { useState } from 'react';
+import AddressForm from '../AddressForm/AddressForm';
 
-// function VisitedCountries() {
-//   const [visitedCountries, setVisitedCountries] = useState([]);
+function VisitedCountry() {
+  const [visitedCountryData, setVisitedCountryData] = useState([]);
 
-//   const handleAddToVisited = (country) => {
-//     setVisitedCountries([...visitedCountries, country]);
-//   };
+  const handleAddToVisitedCountry = (item) => {
+    setVisitedCountryData([...visitedCountryData, item]);
+  };
+  const handleRemoveFromVisitedCountry = (item) => {
+    const filterVisitedCountryData = visitedCountryData.filter(
+      (dataItem) => dataItem.sug.place_type[0] !== item.sug.place_type[0]
+    );
+    console.log(
+      'ka grazina filterVisitedCountryData',
+      filterVisitedCountryData
+    );
+    setVisitedCountryData(filterVisitedCountryData);
+  };
 
-//   const handleRemoveCountry = (index) => {
-//     const updatedCountries = [...visitedCountries];
-//     updatedCountries.splice(index, 1);
-//     setVisitedCountries(updatedCountries);
-//   };
+  return (
+    <div>
+      <AddressForm
+        handleAddToVisitedCountry={handleAddToVisitedCountry}
+        handleRemoveFromVisitedCountry={handleRemoveFromVisitedCountry}
+        visitedCountryData={visitedCountryData}
+      />
+    </div>
+  );
+}
 
-//   return (
-//     <div className="visited-countries">
-//       <h2>Visited Countries</h2>
-//       <AddressForm handleAddToVisited={handleAddToVisited} />
-//       <ul>
-//         {visitedCountries.map((country, index) => (
-//           <li key={index}>
-//             {country}
-//             <button onClick={() => handleRemoveCountry(index)}>Remove</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default VisitedCountries;
-
-/////////////////////////////////////////////////////
-// import './VisitedCountry.scss';
-// import { useState, useContext } from 'react';
-// // import { AppContext } from '../../context/AppContext';
-// import AddressForm from '../AddressForm/AddressForm';
-
-// function VisitedCountry() {
-//   const [address] = useState({
-//     country: '',
-//     latitude: '',
-//     longitude: '',
-//   });
-//   const { visitedCountryData } = useContext();
-
-//   return (
-//     <div>
-//       {visitedCountryData.map((address) => (
-//         <AddressForm key={address.country} />
-//       ))}
-//       <label htmlFor="country">Visited Country</label>
-//       <h3>{address[0]}</h3>
-//     </div>
-//   );
-// }
-
-// export default VisitedCountry;
+export default VisitedCountry;
