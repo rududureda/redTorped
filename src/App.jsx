d928634bc764ea60237f7889e917fa6cb77555bb
rududureda <ranc.reda@gmail.com>
2024-05-01 20:50:10 +0300

    add useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.scss';
 import AddressForm from './components/AddressForm/AddressForm';
 import Map from './components/Map/Map';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  // const [loading, setLoading] = useState(false);
+  // const [countries, setCountries] = useState([]);
   const [address, setAddress] = useState({
     country: '',
     latitude: '',
@@ -23,14 +26,45 @@ function App() {
   const updateCoordinates = (latitude, longitude) => {
     setAddress({ ...address, latitude, longitude });
   };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const response = await axios.get('http://localhost:3000/country'); // Adjust the URL as per your backend endpoint
+  //       setCountries(response.data);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/country');
+        console.log('response', response);
+
+        const data = await response.jason();
+        console.log('data', data);
+      } catch (error) {
+        `Error:${error.message}`;
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <Navbar />
-      <AddressForm 
+      <AddressForm
         onSubmit={handleFormSubmit}
         address={address}
         setAddress={setAddress}
+        // countries={countries}
+        // loading={loading}
       />
       {address.longitude && address.latitude && (
         <Map
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,9 @@
-/////good, original!!! //////////////////////////////////////////////
-// import '../Navbar/Navbar.scss';
-// import { GoogleLogin } from '@react-oauth/google';
-// import { jwtDecode } from 'jwt-decode';
-
-// function Navbar() {
-//   return (
-//     <nav className="nav-container">
-//       <h1>Red Tordeda</h1>
-//       <GoogleLogin
-//         onSuccess={(credentialResponse) => {
-//           const decoded = jwtDecode(credentialResponse?.credential);
-//           console.log("decoded",decoded);
-//         }}
-//         onError={() => {
-//           console.log('Login Failed');
-//         }}
-//       />
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
 
 import '../Navbar/Navbar.scss';
 import { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode'
+
 
 function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
