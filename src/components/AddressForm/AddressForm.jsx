cc84cdac53871d097c3930c65d4d3a4dfdbdb957
rududureda <ranc.reda@gmail.com>
2024-05-02 20:14:34 +0300

    ADD

--- src/components/AddressForm/AddressForm.jsx
+++ src/components/AddressForm/AddressForm.jsx
@@ -1,7 +1,7 @@
 import './addressForm.scss';
 import AutoCompleteInput from '../AutoCompleteInput/AutoCompleteInput';
 import PropTypes from 'prop-types';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 AddressForm.propTypes = {
   address: PropTypes.object.isRequired,
@@ -29,19 +29,43 @@ export default function AddressForm({ address, onSubmit, setAddress }) {
     };
     fetchData();
   }, []);
-  //ToDO
-  // useEffect(() => {
-  //   const uniqueCountries = [...new Set(items)];
-  //   setShowMessage(uniqueCountries.length !== items.length);
-  // }, [items]);
+
   console.log('Item', items);
-  const handleAddCountry = (newCountry) => {
-    if (!items.some((item) => item.country === newCountry.country)) {
-      setItems([...items, newCountry]);
-    } else {
-      setShowMessage(true);
+  const handleAddCountry = async (newCountry) => {
+    try {
+      if (!items.some((item) => item.country === newCountry.country)) {
+        // Make a POST request to your backend to add the new country
+        const response = await fetch('http://localhost:3000/country', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newCountry),
+        });
+
+        if (response.ok) {
+          // If the POST request is successful, add the new country to the items state
+          const data = await response.json();
+          setItems([...items, data]); // Assuming the backend returns the added country object
+        } else {
+          // If the POST request fails, show an error message
+          setShowMessage(true);
+          console.error('Failed to add country:', response.statusText);
+        }
+      } else {
+        setShowMessage(true);
+      }
+    } catch (error) {
+      console.error('Error adding country:', error.message);
     }
   };
+  // const handleAddCountry = (newCountry) => {
+  //   if (!items.some((item) => item.country === newCountry.country)) {
+  //     setItems([...items, newCountry]);
+  //   } else {
+  //     setShowMessage(true);
+  //   }
+  // };
   //TODO
   const handleRemoveCountry = (indexToRemove) => {
     setItems(items.filter((_, index) => index !== indexToRemove));
@@ -96,49 +120,3 @@ export default function AddressForm({ address, onSubmit, setAddress }) {
     </form>
   );
 }
-////////original before 04-22
-
-// import './addressForm.scss';
-// import AutoCompleteInput from '../AutoCompleteInput/AutoCompleteInput';
-// import PropTypes from 'prop-types';
-// import { useState } from 'react';
-
-// AddressForm.propTypes = {
-//   address: PropTypes.object.isRequired,
-//   onSubmit: PropTypes.func.isRequired,
-//   setAddress: PropTypes.func.isRequired,
-// };
-
-// export default function AddressForm({ address, onSubmit, setAddress }) {
-
-//   return (
-//     <form className="form" onSubmit={onSubmit}>
-//       <label htmlFor="address">Address</label>
-//       <AutoCompleteInput setAddress={setAddress} />
-//       <div className="visitedCountry">
-//         <div>
-//             <h2>{address.country}</h2>
-//           ))}
-//         </div>
-//       </div>
-//       <div className="buttons">
-//         <button type="submit" className="confirm-button">
-//           Confirm
-//         </button>
-//         <button
-//           type="reset"
-//           className="reset-button"
-//           onClick={() =>
-//             setAddress({
-//               country: '',
-//               latitude: '',
-//               longitude: '',
-//             })
-//           }
-//         >
-//           Reset
-//         </button>
-//       </div>
-//     </form>
-//   );
-// }
